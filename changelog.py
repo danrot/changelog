6f3660fb090b0515111880b569719cc2a2e2fbc3
Daniel Rotter <daniel.rotter@gmail.com>
2014-07-19 16:07:12 +0200

    retrieved date from tag

--- changelog.py
+++ changelog.py
@@ -4,36 +4,50 @@ import getopt
 import requests
 
 def usage():
-    print("changelog -r <repository_name>")
+    print("changelog -r <repository_name> -f <tag>")
 
 
 def get_pull_requests(repository):
-    # construct url
-    url = "https://api.github.com/repos/" + repository + "/pulls?state=closed?per_page=100" # TODO solve per_page issue
-
     # request information
-    req = requests.get(url)
+    req_pull_requests = requests.get("https://api.github.com/repos/" + repository + "/pulls?state=closed")
 
     # write information to stdout
-    pull_requests = req.json()
+    pull_requests = req_pull_requests.json()
 
     return pull_requests
 
+def get_date_by_tag(repository, tag_name):
+    req_tags = requests.get("https://api.github.com/repos/" + repository + "/tags")
+
+    date = None
+
+    for tag in req_tags.json():
+        if tag.get("name") == tag_name:
+            req_commit = requests.get(tag.get("commit").get("url"))
+            date = req_commit.json().get("commit").get("author").get("date")
+
+    return date
 
 def main():
     # read arguments
     opts = None
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hr:")
+        opts, args = getopt.getopt(sys.argv[1:], "hr:f:")
     except getopt.GetoptError as err:
         print(err)
         usage()
 
-    repository = ""
+    repository = None
+    from_tag = None
 
     for option, value in opts:
         if option == "-r":
             repository = value
+        if option == "-f":
+            from_tag = value
+
+    from_date = get_date_by_tag(repository, from_tag)
+    print(from_date)
 
     pull_requests = get_pull_requests(repository)
 
